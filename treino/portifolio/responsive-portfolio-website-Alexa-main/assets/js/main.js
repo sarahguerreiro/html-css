fa151efcf83fccf5983b0a8495cefa4b67a313ae
sarahguerreiro <sdmguerre@gmail.com>
2023-01-05 22:58:53 -0300

    cola o nome da class q ta recebendo o swiper na primeira linha do code de foi colado em portfolio swiper

--- treino/portifolio/responsive-portfolio-website-Alexa-main/assets/js/main.js
+++ treino/portifolio/responsive-portfolio-website-Alexa-main/assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
     navigation: {
       nextEl: ".swiper-button-next",
