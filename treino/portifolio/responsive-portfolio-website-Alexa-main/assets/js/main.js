a4303beb0a4dcfe04f2131195ed71c5a4295efd8
sarahguerreiro <sdmguerre@gmail.com>
2023-01-12 20:41:16 -0300

    copia todo codigo de potfolioswiper e cola em testimonial trocando a classepra .testimonial__container e troca o nome da let pra swiperTestimonial e a de portfolio swiperPortfolio

--- treino/portifolio/responsive-portfolio-website-Alexa-main/assets/js/main.js
+++ treino/portifolio/responsive-portfolio-website-Alexa-main/assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true, 
 
@@ -106,9 +106,23 @@ let swiper = new Swiper('.portfolio__container', {
     },
 
   });
-
-/*==================== TESTIMONIAL ====================*/
-
+  
+  /*==================== TESTIMONIAL ====================*/
+  let swiperTestimonial = new Swiper('.testimonial__container', {
+      cssMode: true,
+      loop: true, 
+  
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true, 
+      },
+  
+    });
+  
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
