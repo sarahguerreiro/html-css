2edf05905d7d4dfae65b70a27cf7bd7196f949ef
sarahguerreiro <sdmguerre@gmail.com>
2023-01-18 21:03:16 -0300

    tornando o botao de paginacao clicavel

--- treino/portifolio/responsive-portfolio-website-Alexa-main/assets/js/main.js
+++ treino/portifolio/responsive-portfolio-website-Alexa-main/assets/js/main.js
@@ -95,12 +95,15 @@ modalCloses.forEach((modalClose) => {
 
 let swiper = new Swiper(".mySwiper", {
     cssMode: true,
+    loop: true,
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     mousewheel: true,
     keyboard: true,
